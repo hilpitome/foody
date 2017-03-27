00e7a4587c791ac9374ee39fdd38686645ab751e
hilpitome <hilpitome@gmail.com>
2017-03-27 13:40:10 +0300

    food listing

--- app/src/main/java/com/example/hilary/foody/MainActivity.java
+++ app/src/main/java/com/example/hilary/foody/MainActivity.java
@@ -2,12 +2,64 @@ package com.example.hilary.foody;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mConditionTextView;
+    Button mButtonSunny;
+    Button mButtonFoggy;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionReference = mRootRef.child("condition");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mConditionTextView = (TextView) findViewById(R.id.condition);
+        mButtonSunny = (Button) findViewById(R.id.sunny);
+        mButtonFoggy = (Button) findViewById(R.id.foggy);
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mConditionReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String text = (String) dataSnapshot.getValue(String.class);
+
+                mConditionTextView.setText(text);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        mButtonSunny.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConditionReference.setValue("oh sunny day");
+            }
+        });
+        mButtonFoggy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mConditionReference.setValue("oh foggy day");
+            }
+        });
+
     }
 }
