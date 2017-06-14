29d4df4a8bca868affdd72688e1accc2445646dd
hilpitome <hilpitome@gmail.com>
2017-06-15 00:58:59 +0300

    init initializing drawer menu

---
+++ app/src/main/java/com/example/hilary/foody/LatestFoodFragment.java
@@ -0,0 +1,125 @@
+package com.example.hilary.foody;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.hilary.foody.adapters.MyFirebaseAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hilary on 6/6/17.
+ */
+
+public class LatestFoodFragment extends Fragment {
+    private static final String SAVED_ADAPTER_ITEMS = "saved_adapter_items";
+    private final static String SAVED_ADAPTER_KEYS = "saved_adapter_keys";
+    TextView title;
+    TextView price;
+    TextView description;
+    ImageView foodImage;
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mFoodReference = mRootRef.child("foods");
+    Query mQuery;
+    ArrayList<Food> mAdapterItems;
+    ArrayList<String> mAdapterKeys;
+    RecyclerView mRecyclerView;
+    MyFirebaseAdapter mMyAdapter;
+    ProgressDialog mProgressBar;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_latest_food,
+                container, false);
+        intitializeView(view);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.foodListRecyclerView);
+        setupRecyclerview();
+
+        return view;
+    }
+
+    public  void  intitializeView(View v){
+        title = (TextView) v.findViewById(R.id.title);
+        description = (TextView) v.findViewById(R.id.description);
+
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mProgressBar = new ProgressDialog(getActivity());
+        handleInstanceState(savedInstanceState);
+        setupFirebase();
+
+    }
+    private void setupFirebase() {
+        showProgress();
+        mQuery = mFoodReference.limitToLast(10);
+        mQuery.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                removeProgress(); //Remove progress bar when data has been fully downloaded
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
+    }
+    private void handleInstanceState(Bundle savedInstanceState) {
+
+        if (savedInstanceState != null &&
+            savedInstanceState.containsKey(SAVED_ADAPTER_ITEMS) &&
+            savedInstanceState.containsKey(SAVED_ADAPTER_KEYS)) {
+            mAdapterItems = Parcels.unwrap(savedInstanceState.getParcelable(SAVED_ADAPTER_ITEMS));
+            mAdapterKeys = savedInstanceState.getStringArrayList(SAVED_ADAPTER_KEYS);
+        } else {
+            mAdapterItems = new ArrayList<Food>();
+            mAdapterKeys = new ArrayList<String>();
+        }
+    }
+    private void setupRecyclerview() {
+
+        mMyAdapter = new MyFirebaseAdapter(mQuery, Food.class, mAdapterItems, mAdapterKeys);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mMyAdapter);
+    };
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMyAdapter.destroy();
+
+    }
+    public void showProgress(){
+        mProgressBar.setMessage("please wait");
+        mProgressBar.show();
+    }
+    public void removeProgress(){
+        mProgressBar.dismiss();
+    }
+}
--- app/src/main/java/com/example/hilary/foody/MainActivity.java
+++ app/src/main/java/com/example/hilary/foody/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.hilary.foody;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,22 +35,33 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String SAVED_ADAPTER_ITEMS = "SAVED_ADAPTER_ITEMS";
-    private final static String SAVED_ADAPTER_KEYS = "SAVED_ADAPTER_KEYS";
-    TextView title;
-    TextView price;
-    TextView description;
-    ImageView foodImage;
-    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference mFoodReference = mRootRef.child("foods");
-    Query mQuery;
-    ArrayList<Food> mAdapterItems;
-    ArrayList<String> mAdapterKeys;
-    RecyclerView mRecyclerView;
-    MyFirebaseAdapter mMyAdapter;
-    View mProgressBar;
-    private FirebaseAuth mAuth;
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private View navHeader;
+    private ImageView imgNavHeaderBg, imgProfile;
+    private TextView txtEmail;
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
 
+    private FirebaseAuth mAuth;
+    // index to identify current nav menu item
+    public static int navItemIndex = 0;
+
+    // tags used to attach the fragments
+    private static final String TAG_LATEST = "Latest";
+    private static final String TAG_NOTIFICATIONS = "notifications";
+    private static final String TAG_ABOUT_US = "about_us";
+    private static final String TAG_PAYMENTS = "payments";
+    private static final String TAG_ACCOUNT = "account";
+    private static final String TAG_HELP = "help";
+    public static String CURRENT_TAG = TAG_LATEST;
+
+    // toolbar titles respected to selected nav menu item
+    private String[] activityTitles;
+
+    // flag to load home fragment when user presses back key
+    private boolean shouldLoadHomeFragOnBackPress = true;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,67 +69,252 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            //getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayShowHomeEnabled(false);
+        }
         if (currentUser!= null){
-            title = (TextView) findViewById(R.id.title);
-            description = (TextView) findViewById(R.id.description);
-            mProgressBar = findViewById(R.id.progressBar);
+            mHandler = new Handler();
+            drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            navigationView = (NavigationView) findViewById(R.id.nav_view);
+            fab = (FloatingActionButton) findViewById(R.id.fab);
 
-            handleInstanceState(savedInstanceState);
-            setupFirebase();
-            setupRecyclerview();
+            // Navigation view header
+            navHeader = navigationView.getHeaderView(0);
+            txtEmail = (TextView) navHeader.findViewById(R.id.email);
+            imgNavHeaderBg = (ImageView) navHeader.findViewById(R.id.img_header_bg);
+            imgProfile = (ImageView) navHeader.findViewById(R.id.img_profile);
+
+            // load toolbar titles from string resources
+            activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
+
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+
+            // load nav menu header data
+            loadNavHeader();
+
+            // initializing navigation menu
+            setUpNavigationView();
+
+            if (savedInstanceState == null) {
+                navItemIndex = 0;
+                CURRENT_TAG = TAG_LATEST;
+                loadHomeFragment();
+            }
         } else {
             Intent i = new Intent(MainActivity.this, SignUpAcitivity.class);
             startActivity(i);
         }
-
     }
 
+    /***
+     * Load navigation menu header information
+     * like background image, profile image
+     * email, notifications action view (dot)
+     */
+    private void loadNavHeader() {
+        // name, website
+        txtEmail.setText("hila@foody.com");
 
 
+//        // loading header background image
+//        Glide.with(this).load(urlNavHeaderBg)
+//                .crossFade()
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                .into(imgNavHeaderBg);
+//
+//        // Loading profile image
+//        Glide.with(this).load(urlProfileImg)
+//                .crossFade()
+//                .thumbnail(0.5f)
+//                .bitmapTransform(new CircleTransform(this))
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                .into(imgProfile);
 
-    private void setupFirebase() {
-        mQuery = mFoodReference.limitToLast(10);
-        mQuery.addValueEventListener(new ValueEventListener() {
+        // showing dot next to notifications label
+        //navigationView.getMenu().getItem(3).setActionView(R.layout.menu_dot);
+    }
+    /***
+     * Returns respected fragment that user
+     * selected from navigation menu
+     */
+    private void loadHomeFragment() {
+        // selecting appropriate nav menu item
+        selectNavMenu();
 
+        // set toolbar title
+        setToolbarTitle();
+
+        // if user select the current navigation menu again, don't do anything
+        // just close the navigation drawer
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
+            drawer.closeDrawers();
+
+            // show or hide the fab button
+            toggleFab();
+            return;
+        }
+
+        // Sometimes, when fragment has huge data, screen seems hanging
+        // when switching between navigation menus
+        // So using runnable, the fragment is loaded with cross fade effect
+        // This effect can be seen in GMail app
+        Runnable mPendingRunnable = new Runnable() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                mProgressBar.setVisibility(View.GONE); //Remove progress bar when data has been fully downloaded
+            public void run() {
+                // update the main content by replacing fragments
+                Fragment fragment = getHomeFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
             }
+        };
 
+        // If mPendingRunnable is not null, then add to the message queue
+        if (mPendingRunnable != null) {
+            mHandler.post(mPendingRunnable);
+        }
+
+        // show or hide the fab button
+        toggleFab();
+
+        //Closing drawer on item click
+        drawer.closeDrawers();
+
+        // refresh toolbar menu
+        invalidateOptionsMenu();
+    }
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
+    private void selectNavMenu() {
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+    private void setUpNavigationView() {
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
 
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.nav_home:
+                        navItemIndex = 0;
+                        CURRENT_TAG = TAG_LATEST;
+                        break;
+//                    case R.id.nav_photos:
+//                        navItemIndex = 1;
+//                        CURRENT_TAG = TAG_PHOTOS;
+//                        break;
+//                    case R.id.nav_movies:
+//                        navItemIndex = 2;
+//                        CURRENT_TAG = TAG_MOVIES;
+//                        break;
+//                    case R.id.nav_notifications:
+//                        navItemIndex = 3;
+//                        CURRENT_TAG = TAG_NOTIFICATIONS;
+//                        break;
+//                    case R.id.nav_settings:
+//                        navItemIndex = 4;
+//                        CURRENT_TAG = TAG_SETTINGS;
+//                        break;
+//                    case R.id.nav_about_us:
+//                        // launch new intent instead of loading fragment
+//                        startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
+//                        drawer.closeDrawers();
+//                        return true;
+//                    case R.id.nav_privacy_policy:
+//                        // launch new intent instead of loading fragment
+//                        startActivity(new Intent(MainActivity.this, PrivacyPolicyActivity.class));
+//                        drawer.closeDrawers();
+//                        return true;
+                    default:
+                        navItemIndex = 0;
+                }
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+
+                loadHomeFragment();
+
+                return true;
             }
-
         });
     }
-    private void handleInstanceState(Bundle savedInstanceState) {
-
-        if (savedInstanceState != null &&
-                savedInstanceState.containsKey(SAVED_ADAPTER_ITEMS) &&
-                savedInstanceState.containsKey(SAVED_ADAPTER_KEYS)) {
-            mAdapterItems = Parcels.unwrap(savedInstanceState.getParcelable(SAVED_ADAPTER_ITEMS));
-            mAdapterKeys = savedInstanceState.getStringArrayList(SAVED_ADAPTER_KEYS);
-        } else {
-            mAdapterItems = new ArrayList<Food>();
-            mAdapterKeys = new ArrayList<String>();
+    private Fragment getHomeFragment() {
+        switch (navItemIndex) {
+            case 0:
+                // home
+                LatestFoodFragment homeFragment = new LatestFoodFragment();
+                return homeFragment;
+//            case 1:
+//                // photos
+//                PhotosFragment photosFragment = new PhotosFragment();
+//                return photosFragment;
+//            case 2:
+//                // movies fragment
+//                MoviesFragment moviesFragment = new MoviesFragment();
+//                return moviesFragment;
+//            case 3:
+//                // notifications fragment
+//                NotificationsFragment notificationsFragment = new NotificationsFragment();
+//                return notificationsFragment;
+//
+//            case 4:
+//                // settings fragment
+//                SettingsFragment settingsFragment = new SettingsFragment();
+//                return settingsFragment;
+            default:
+                return new LatestFoodFragment();
         }
     }
-    private void setupRecyclerview() {
-        mRecyclerView = (RecyclerView) findViewById(R.id.foodListRecyclerView);
-        mMyAdapter = new MyFirebaseAdapter(mQuery, Food.class, mAdapterItems, mAdapterKeys);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setAdapter(mMyAdapter);
-    };
-
-
-
-
-
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mMyAdapter.destroy();
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawers();
+            return;
+        }
 
+        // This code loads home fragment when back key is pressed
+        // when user is in other fragment than home
+        if (shouldLoadHomeFragOnBackPress) {
+            // checking if user is on other navigation menu
+            // rather than home
+            if (navItemIndex != 0) {
+                navItemIndex = 0;
+                CURRENT_TAG = TAG_LATEST;
+                loadHomeFragment();
+                return;
+            }
+        }
+
+        super.onBackPressed();
     }
+    // show or hide the fab
+    private void toggleFab() {
+        if (navItemIndex == 0)
+            fab.show();
+        else
+            fab.hide();
+    }
+
 }
